1bdf718ac9d978d2fc41658f5bc5f38aaaf9904a
jim <wasd2144@hotmail.com>
2018-05-31 12:14:28 +0800

    init

---
+++ api.js
@@ -0,0 +1,73 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed: Error: Cannot use the decorators and decorators-legacy plugin together\n    at validatePlugins (/Users/jim/Documents/GitHub/ant-design-pro/node_modules/@babel/parser/lib/index.js:10292:11)\n    at getParser (/Users/jim/Documents/GitHub/ant-design-pro/node_modules/@babel/parser/lib/index.js:10342:5)\n    at parse (/Users/jim/Documents/GitHub/ant-design-pro/node_modules/@babel/parser/lib/index.js:10326:12)\n    at parser (/Users/jim/Documents/GitHub/ant-design-pro/node_modules/@babel/core/lib/transformation/normalize-file.js:137:34)\n    at normalizeFile (/Users/jim/Documents/GitHub/ant-design-pro/node_modules/@babel/core/lib/transformation/normalize-file.js:109:11)\n    at runSync (/Users/jim/Documents/GitHub/ant-design-pro/node_modules/@babel/core/lib/transformation/index.js:44:43)\n    at runAsync (/Users/jim/Documents/GitHub/ant-design-pro/node_modules/@babel/core/lib/transformation/index.js:35:14)\n    at process.nextTick (/Users/jim/Documents/GitHub/ant-design-pro/node_modules/@babel/core/lib/transform.js:34:34)\n    at _combinedTickCallback (internal/process/next_tick.js:131:7)\n    at process._tickCallback (internal/process/next_tick.js:180:9)");
+
+/***/ })
+/******/ ]);
